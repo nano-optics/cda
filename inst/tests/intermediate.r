c6ff429ff7476f5c6e30137bfce3fda9c4e4139f
baptiste <baptiste@baptistes-MacBook-Pro-2.local>
2013-10-26 18:15:46 -0300

    tests of intermediate results

--- inst/tests/intermediate.r
+++ inst/tests/intermediate.r
@@ -34,25 +34,6 @@ test_that("the inverse polarisability is the same as earlier versions", {
   expect_equal(invalpha,.invalpha)
 })
 
-## cheap averaging
-
-nodes <- rbind(c(1/2, 0), # +x is phi=0, psi=0
-               c(1/2, 1/4), # +y is phi=pi/2, psi=0
-               c(1, 1/4)) # +z is phi=pi/2, psi=pi/2
-
-res <- cd$circular_dichroism_spectrum2(kn, invalpha, cluster$r, cluster$angles, 
-                                       as.matrix(nodes),
-                                       as.integer(TRUE), as.integer(TRUE))
-
-.res <- structure(c(7629.71366621336, 18093.6538420437, 6965.12045086035, 
-                   12499.019321288, -0.216853598901253, -533.349596245196, 2.67479400633874, 
-                   -361.940147617599), .Dim = c(2L, 4L))
-
-
-test_that("the result of cheap orientation averaging is the same as earlier versions", {
-  expect_equal(res,.res)
-})
-
 
 Beta <- matrix(invalpha[1,], nrow=3)
 
@@ -69,5 +50,27 @@ A <- cda$interaction_matrix(cluster$r, kn[1], Beta, cluster$angles, 1)
                   0+0i, 0+0i, 2.89922059965679e-05-3.56779387878792e-05i), .Dim = c(6L, 
                                                                                     6L))
 
-all.equal(A, .A)
-max(Re(A))
+test_that("the interaction matrix is the same as earlier versions", {
+  expect_equal(A, .A)
+})
+
+## cheap averaging
+
+nodes <- rbind(c(1/2, 0), # +x is phi=0, psi=0
+               c(1/2, 1/4), # +y is phi=pi/2, psi=0
+               c(1, 1/4)) # +z is phi=pi/2, psi=pi/2
+
+res <- cd$circular_dichroism_spectrum2(kn, invalpha, cluster$r, cluster$angles, 
+                                       as.matrix(nodes),
+                                       as.integer(TRUE), as.integer(FALSE))
+
+.res <- structure(c(7629.71366621336, 18093.6538420437, 6965.12045086035, 
+                   12499.019321288, -0.216853598901253, -533.349596245196, 2.67479400633874, 
+                   -361.940147617599), .Dim = c(2L, 4L))
+
+
+test_that("the result of cheap orientation averaging is the same as earlier versions", {
+  expect_equal(res,.res)
+})
+
+
