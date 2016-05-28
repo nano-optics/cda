3acd3ec72a5e5b83ac1d223cb3d6c0904f6719d5
baptiste <baptiste.auguie@gmail.com>
2016-05-28 22:52:47 +1200

    h

--- src/oos.h
+++ src/oos.h
@@ -2,10 +2,19 @@
 #define _OOS_H
 
 arma::colvec cpp_iterate_field(const arma::mat& R,
-                               const double kn, const double medium,
-                               const arma::cx_mat& Ein,
-                               const arma::cx_cube& AlphaBlocks,
-                               arma::cx_mat& Eloc,
-                               arma::cx_mat& P);
+  const double kn, const double medium,
+  const arma::cx_mat& Ein,
+  const arma::cx_cube& AlphaBlocks,
+  arma::cx_mat& Eloc,
+  arma::cx_mat& P);
+
+  arma::rowvec cpp_oa_oos(const arma::mat& R,
+    const arma::cx_cube& AlphaBlocks,
+    const double kn, const double medium,
+    const arma::mat& IncidenceNodes,
+    const arma::colvec& IncidenceWeights,
+    const arma::mat& ScatteringNodes,
+    const arma::colvec& ScatteringWeights,
+    const int maxiter, const double tol);
 
 #endif
