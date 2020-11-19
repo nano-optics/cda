7e326b31ccf505fac26370781d12954b24d73066
baptiste auguie <baptiste.auguie@gmail.com>
2020-11-20 08:06:00 +1300

    CI

--- R/quadrature.r
+++ R/quadrature.r
@@ -16,7 +16,7 @@
 ##' @family low_level quadrature
 ##' @author baptiste Auguie
 quadrature_sphere <- function(Nq = 30,
-    quadrature = c("qmc", "gl", "cheap", "random"),
+    quadrature = c("qmc", "gl", "cheap", "random", 'grid','grid2', 'fibonacci', 'fibonacci2'),
     init = TRUE){
 
   quadrature <- match.arg(quadrature)
@@ -48,36 +48,122 @@ quadrature_sphere <- function(Nq = 30,
 
   if(quadrature == "random"){ # monte-carlo with random points
 
-    alpha <- runif(Nq, -pi, pi) # uniform [-pi,pi]
+    alpha <- runif(Nq, 0, 2*pi) # uniform [-pi,pi]
     beta <- acos(runif(Nq, -1, 1)) # cos-uniform [-1,1]
     nodes <- rbind(alpha=alpha, beta=beta, gamma=0)
     weights <- rep(1/ncol(nodes), ncol(nodes))
     return(list(nodes=nodes, weights=weights))
   }
-
-  if(quadrature == "gl"){ #  gauss legendre
+  
+  if(quadrature == "grid"){ # grid in acos beta and alpha
     # might have slightly more than N total points
-    rndN <- ceiling(sqrt(Nq/2))
+    # N = 2P^2 -> P = [sqrt(N/2)]
+    # now check +/-1 on each one and pick best combo
+    P <- sqrt(Nq/2)
+    opts <- expand.grid(N1 = round(P) + seq(-1,1), N2=2*round(P) + seq(-1,1))
+    prod <- opts$N1 * opts$N2
+    best <- which.min(abs(Nq - (prod+2))) # add two poles
+    Nbeta <- opts$N1[best]
+    Nalpha <- opts$N2[best]
+    
+    alpha <- seq(0, 2*pi *(1 - 1/Nalpha), by = 2*pi/Nalpha)
+    beta <- acos(seq(-1+1/(2*Nbeta+1),1-1/(2*Nbeta+1), length.out = Nbeta)) 
+    # cos-uniform ]-1,1[ exclude poles as otherwise many points there
+    # nodes <- rbind(expand.grid(alpha=alpha, beta=beta, gamma=0),
+    #                c(0,0,0), c(0, pi, 0)) # add two poles
+    nodes <- expand.grid(alpha=alpha, beta=beta, gamma=0)
+    weights <- rep(1/nrow(nodes), nrow(nodes))
+    
+    return(list(nodes=t(nodes), weights=weights))
+  }
+  if(quadrature == "grid2"){ # grid in acos beta and alpha
+    # might have slightly more than N total points
+    # N = 2P^2 -> P = [sqrt(N/2)]
+    # now check +/-1 on each one and pick best combo
+    P <- sqrt(Nq/2)
+    opts <- expand.grid(N1 = round(P) + seq(-1,1), N2=2*round(P) + seq(-1,1))
+    prod <- opts$N1 * opts$N2
+    best <- which.min(abs(Nq - (prod+2))) # add two poles
+    Nbeta <- opts$N1[best]
+    Nalpha <- opts$N2[best]
+    
+    alpha <- seq(0, 2*pi *(1 - 1/Nalpha), by = 2*pi/Nalpha)
+    # beta <- acos(seq(-1+1/(2*Nbeta+1),1-1/(2*Nbeta+1), length.out = Nbeta)) 
+    step <- 2/Nbeta
+    beta <- acos(seq(-1+step,1-step, length.out = Nbeta)) 
+    # cos-uniform ]-1,1[ exclude poles as otherwise many points there
+    # nodes <- rbind(expand.grid(alpha=alpha, beta=beta, gamma=0),
+    #                c(0,0,0), c(0, pi, 0)) # add two poles
+    nodes <- expand.grid(alpha=alpha, beta=beta, gamma=0)
+    weights <- rep(1/nrow(nodes), nrow(nodes))
+    
+    return(list(nodes=t(nodes), weights=weights))
+  }
+  
+  
+  if(quadrature == "fibonacci"){ # spiral
+    
+    N0 <- Nq
+    if(Nq%%2 == 1) N0 <- Nq+1
+    P <- (N0-1)/2
+    ii <- seq(-P,P,by=1)
+    # asin(2*ii/N0) latitude = asin(x) -> colatitude = pi/2 - asin(x)
+    beta <- pi/2 - asin(2*ii/N0)
+    golden <- (1+sqrt(5))/2
+    alpha <- (2*pi*ii/golden) %% (2*pi)
+    
+    nodes <- rbind(alpha=alpha, beta=beta, gamma=0)
+    weights <- rep(1/ncol(nodes), ncol(nodes))
+    return(list(nodes=nodes, weights=weights))
+  }
+  
+  
+  if(quadrature == "fibonacci2"){ # spiral
+    
+    N0 <- Nq
+    if(Nq%%2 == 1) N0 <- Nq+1
+    
+    ii <- seq(0,N0,by=1)
+    
+    beta <- acos(1 - (2*ii+1)/N0)
+    golden <- (1+sqrt(5))/2
+    alpha <- (2*pi*ii/golden) %% (2*pi)
+    
+    nodes <- rbind(alpha=alpha, beta=beta, gamma=0)
+    weights <- rep(1/ncol(nodes), ncol(nodes))
+    return(list(nodes=nodes, weights=weights))
+  }
+  
+
+  if(quadrature == "gl"){ #  gauss legendre along cos beta, grid alpha
+    # might have slightly more than N total points
+    # N = 2P^2 -> P = [sqrt(N/2)]
+    # now check +/-1 on each one and pick best combo
+    P <- sqrt(Nq/2)
+    opts <- expand.grid(N1 = round(P) + seq(-1,1), N2=2*round(P) + seq(-1,1))
+    prod <- opts$N1 * opts$N2
+    best <- which.min(abs(Nq - prod))
+    Nbeta <- opts$N1[best]
+    Nalpha <- opts$N2[best]
 
     # scale the coordinates from (-1, 1) to (0, 2pi)
     # and cos beta in (-1, 1) resp. (that one unnecessary btw)
-    phi1=0; phi2=2*pi; psi1=-1; psi2=1;
-    C1 = (phi2 - phi1) / 2;  D1 = (phi2 + phi1) / 2;
+    psi1=-1; psi2=1;
     C2 = (psi2 - psi1) / 2; D2 = (psi2 + psi1) / 2;
+    
+    GL_alpha <- seq(0, 2*pi *(1 - 1/Nalpha), by = 2*pi/Nalpha)
+    GL_beta <- statmod::gauss.quad(Nbeta)
 
-    GL_alpha <- statmod::gauss.quad(2*rndN)
-    GL_beta <- statmod::gauss.quad(rndN)
-
-    alpha = GL_alpha$nodes*C1 + D1
+    alpha = GL_alpha
     beta  = acos(GL_beta$nodes*C2 + D2)
 
     # grid of angles, gamma is constant here
     nodes <- expand.grid(alpha=alpha, beta=beta, gamma=0)
     # corresponding weights for 1D quadrature
-    weights <- expand.grid(alpha=GL_alpha$weights,
+    weights <- expand.grid(alpha=rep(1/Nalpha, Nalpha),
                            beta=GL_beta$weights)
     # combine the weigths
-    weights <- C1 * C2 / (4*pi) * weights$alpha * weights$beta
+    weights <- C2 / 2 * weights$alpha * weights$beta
 
     return(list(nodes=t(as.matrix(nodes)), weights=weights))
   }
--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 
-[![Build Status](https://travis-ci.org/plasmonics/cda.png?branch=master)](https://travis-ci.org/plasmonics/cda) ![](http://www.r-pkg.org/badges/version/cda) ![](http://cranlogs.r-pkg.org/badges/grand-total/cda)
+[![R build
+status](https://github.com/nano-optics/cda/workflows/R-CMD-check/badge.svg)](https://github.com/nano-optics/cda/actions)
+![](http://www.r-pkg.org/badges/version/cda)
+![](http://cranlogs.r-pkg.org/badges/grand-total/cda)
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
-cda package
-===========
 
-Coupled-dipole simulations for electromagnetic scattering of light by sub-wavelength particles in arbitrary 3-dimensional configurations. Scattering and absorption spectra are simulated by inversion of the interaction matrix, or by an order-of-scattering approximation scheme. High-level functions are provided to simulate spectra with varying angles of incidence, as well as with full angular averaging.
+# cda package
+
+Coupled-dipole simulations for electromagnetic scattering of light by
+sub-wavelength particles in arbitrary 3-dimensional configurations.
+Scattering and absorption spectra are simulated by inversion of the
+interaction matrix, or by an order-of-scattering approximation scheme.
+High-level functions are provided to simulate spectra with varying
+angles of incidence, as well as with full angular averaging.
 
 <img src="overview.png" width="600" style="display: block; margin: auto;" />
 
